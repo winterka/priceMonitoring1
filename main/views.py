6a391728d448f0667c1e48fcc1efeea31130fb74
formorter <72940774+formorter@users.noreply.github.com>
2021-06-04 22:57:56 +0300

    html файл бутстрап
    изменена модель, добавлено тестовое оформление сайта

--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return HttpResponse("Ну собсна страница как она есть")
+    return render(request, 'main/index.html')
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('./')
+    else:
+        return HttpResponseRedirect('/account/invalid')
