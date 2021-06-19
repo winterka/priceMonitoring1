abc0f4fb6588595bd19ae791ddfe0415d776ee00
Misha <formorter@yandex.ru>
2021-06-19 22:58:21 +0300

    типизация, изменение модели

--- main/models.py
+++ main/models.py
@@ -4,8 +4,7 @@ from django.core.validators import MinValueValidator
 
 class Complete(models.Model):
 	name = models.CharField(max_length=200)
-	pub_date = models.BigIntegerField(blank=True, default=0)
-	price = models. BigIntegerField(blank=True, default=0)
+	price = models. FloatField(blank=True, default=0)
 
 	class Meta:
 		verbose_name = 'Complete Price'
@@ -13,6 +12,6 @@ class Complete(models.Model):
 
 
 	def __str__(self):
-		return f'Name: {self.name}, date: {self.pub_date}, price: {self.price}'
+		return f'Name: {self.name}, price: {self.price}'
 
   
